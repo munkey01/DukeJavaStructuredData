2921a9404800fe392dd74beee685985be6225807
munkey01 <munkey01@users.noreply.github.com>
2017-01-26 10:48:45 -0500

    Improved comments and readability

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -20,8 +20,8 @@ public class CaesarCipher {
     }
 
     void setShift(int key) {
-        while(key > 26) {
-            key = key-26; //adjust key to within alphabetic length range
+        while (key > 26) {
+            key = key-26;                   //adjust key to within range of alphabet length
         }
         shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
     }
@@ -31,9 +31,10 @@ public class CaesarCipher {
     }
 
     int[] findLetterFrequency(String message) {
-        int[] letterCounts = new int[26];
+        int[] letterCounts = new int[26];               //stores counts for each letter occurring in message
         int length = message.length();
-        for(int i = 0; i < length; i++) {
+
+        for (int i = 0; i < length; i++) {
             char currentLetter = message.charAt(i);
             int letterIndexInAlphabet = alphabet.indexOf(Character.toUpperCase(currentLetter));
             if (letterIndexInAlphabet != -1) {
@@ -45,17 +46,17 @@ public class CaesarCipher {
 
     int findUnknownKey(String message) {
         int[] letterCounts = findLetterFrequency(message);
-
-        //find letter of highest count
         int max = 0;
         char mostCommonLetter = 'A';
-        for (int i = 0; i < 25; i++) {
+
+        for (int i = 0; i < 25; i++) {                  //find most common letter in message
             if (letterCounts[i] > max) {
                 max = letterCounts[i];
                 mostCommonLetter = alphabet.charAt(i);
             }
         }
-        //assume highest count represents decrypted 'e', shift alphabet appropriately
+
+        /* Assume highest count represents decrypted 'e', shift alphabet appropriately */
         int key = alphabet.indexOf(mostCommonLetter) - alphabet.indexOf('E');
         System.out.println("Most common letter: " + mostCommonLetter + "\nCalculated key: " + key);
         return key;
@@ -63,25 +64,28 @@ public class CaesarCipher {
 
     String decryptKeyUnknown(String message) {
         int key = findUnknownKey(message);
+
         System.out.println("Calculated shift: " + key);
         return decrypt(message, key);
     }
 
     String encrypt(String message, int key) {
-        setShift(key);
         StringBuilder encryptedStr = new StringBuilder();
+        setShift(key);
 
-        for(int i = 0; i < message.length(); i++) {
+        for (int i = 0; i < message.length(); i++) {
             char currentLetter = message.charAt(i);
-            //find index of letter in shifted alphabet, must do this as uppercase version
-            int indexOfChar = alphabet.indexOf(Character.toUpperCase(currentLetter));
-            if(indexOfChar == -1) {
+            char charToAdd;
+            int indexOfChar = alphabet.indexOf(Character.toUpperCase(currentLetter));           //find index of letter in alphabet
+
+            if (indexOfChar == -1) {
                 encryptedStr.append(currentLetter);
                 continue;
             }
-            char charToAdd = shiftedAlphabet.charAt(indexOfChar);
-            if(Character.isLowerCase(currentLetter)) {
-                //add lowercase shifted letter to encrypted string if original was lowercase
+            charToAdd = shiftedAlphabet.charAt(indexOfChar);
+            if (Character.isLowerCase(currentLetter)) {
+
+                /* Add lowercase shifted letter to encrypted string if original was lowercase */
                 charToAdd = Character.toLowerCase(charToAdd);
             }
             encryptedStr.append(charToAdd);
@@ -93,13 +97,14 @@ public class CaesarCipher {
     String encryptTwoKeys(String message, int key1, int key2) {
         int length = message.length();
         StringBuilder encryptedStr = new StringBuilder();
-        for(int i = 0; i < length; i++) {
-            char currentLetter = message.charAt(i);
-            String encryptedLetter = null;
-            if(i % 2 == 0) {
+        String encryptedLetter = new String();
+        char currentLetter;
+
+        for (int i = 0; i < length; i++) {
+            currentLetter = message.charAt(i);
+            if (i % 2 == 0) {
                 encryptedLetter = encrypt(Character.toString(currentLetter), key1);
-            }
-            else if(i % 2 == 1) {
+            } else if (i % 2 == 1) {
                 encryptedLetter = encrypt(Character.toString(currentLetter), key2);
             }
             encryptedStr.append(encryptedLetter);
@@ -108,19 +113,19 @@ public class CaesarCipher {
     }
 
     String decrypt(String encrypted, int key) {
-        return encrypt(encrypted, (26-key)); //decrypting is just a shifted encrypt
+        return encrypt(encrypted, (26-key));            //decrypting is just a shifted encrypt
     }
 
     String decryptTwoKeysUnknown(String encryptedStr) {
         StringBuilder string1 = new StringBuilder();
         StringBuilder string2 = new StringBuilder();
+        char currentLetter;
 
-        for(int i = 0; i < encryptedStr.length()-1; i++) {
-            char currentLetter = encryptedStr.charAt(i);
-            if(i % 2 == 0) {
+        for (int i = 0; i < encryptedStr.length()-1; i++) {
+            currentLetter = encryptedStr.charAt(i);
+            if (i % 2 == 0) {
                 string1.append(currentLetter);
-            }
-            else if(i % 2 == 1) {
+            } else if(i % 2 == 1) {
                 string2.append(currentLetter);
             }
         }
