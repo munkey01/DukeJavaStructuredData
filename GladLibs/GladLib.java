d93419646e1567e1411ecec83a6a4c2c58958f54
jgrant <joeydgrant@gmail.com>
2017-01-31 16:02:02 -0500

    GladLibs completed (round 1). Testing incomplete.
    Still needs: directory fixes

---
+++ GladLibs/GladLib.java
@@ -0,0 +1,98 @@
+/**
+ * Created by jgrant on 1/27/2017.
+ * This class uses a story template to create a randomized story
+ * referred to as a GladLib (or a MadLib).
+ */
+
+package GladLibs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GladLib {
+
+    private final String path = "\\data\\";
+    private final String nounsFile = "nouns.txt";
+    private final String verbsFile = "verbs.txt";
+    private final String adjFile = "adjectives.txt";
+    private final String storyTemplateFileName;
+    private String storyTemplate;
+    private final ArrayList<String> nouns = new ArrayList<>();
+    private final ArrayList<String> verbs = new ArrayList<>();
+    private final ArrayList<String> adjectives = new ArrayList<>();
+
+    GladLib() {
+        this("story_template.txt");
+    }
+
+    GladLib(String templateFile) {
+        storyTemplateFileName = templateFile;
+        try {
+            loadWords(nouns, nounsFile);
+            loadWords(verbs, verbsFile);
+            loadWords(adjectives, adjFile);
+            loadStoryTemplate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFile(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, "Cp1252");
+    }
+
+    private void loadWords(ArrayList<String> list, String filename) throws IOException  {
+        String fileAsString = readFile(path + filename);
+        Collections.addAll(list, fileAsString.split("\\s"));
+    }
+
+    private void loadStoryTemplate() throws IOException {
+        storyTemplate = readFile(path + storyTemplateFileName);
+    }
+
+    /* Will pick random word from given type and return the word as a string. */
+    private String pickRandomWord(ArrayList<String> wordList) {
+        Random rand = new Random();
+        int size = wordList.size();
+        int index = rand.nextInt(size);
+        return wordList.get(index);
+    }
+
+    /* Takes story template, calls pickRandomWord() for each tagged word. Replaces word
+     * with return value from pickRandomWord. Include catch for error from pickRandomWord */
+    protected String createStory() {
+        StringBuilder story = new StringBuilder();
+        for (String word : storyTemplate.split("\\s")) {
+            switch (word) {
+                case "<noun>" :
+                    story.append(pickRandomWord(nouns));
+                    continue;
+                case "<verb>" :
+                    story.append(pickRandomWord(verbs));
+                    continue;
+                case "<adjective>" :
+                    story.append(pickRandomWord(adjectives));
+                    continue;
+                default:
+                    story.append(word);
+            }
+        }
+        return story.toString();
+    }
+
+    /* Will load words from preset file name. */
+    /*
+    private void loadFileByLines(ArrayList<String> currentList, String currentFileName) throws FileNotFoundException {
+        Scanner scanLines = new Scanner(new File(path + currentFileName));
+        while (scanLines.hasNextLine()) {           //grab line of file, send it to loadWords for splitting and loading
+            loadWords(currentList, scanLines.nextLine());
+        }
+        scanLines.close();
+    } */
+
+}
--- GladLibs/testGladLibs.java
+++ GladLibs/testGladLibs.java
@@ -3,5 +3,13 @@ package GladLibs;
 /**
  * Created by jgrant on 1/27/2017.
  */
+
+
 public class testGladLibs {
+
+    public static void main(String[] args) {
+        GladLib glad = new GladLib();
+        System.out.println(glad.createStory());
+    }
+
 }
